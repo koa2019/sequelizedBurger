6d446e5a9762563c556a95bcc025a9ce7bf4ae4b
koa2019 <no-reply>
2019-09-28 23:37:43 -0700

    altered model burger.js for sequelize node library

--- models/burger.js
+++ models/burger.js
@@ -1,36 +1,19 @@
-// import orm.js from config folder
-// orm interacts with database  
-var orm = require('../config/orm');
+module.exports = function(sequelize, Datatypes) {
 
-// creating a burger model that will call orm functions 
-// function uses burger specific input for the orm
-var burger = {
-    // define function allows us to pass the callback value to another file
-    selectAll: function(cb) {
-        orm.selectAll('burgers', function(res) {
-            // if (err) {
-            //     console.log(err);
-            // }
-            cb(res);
-        });
-    },
-    // cols & vals are arrays
-    insertOne: function(cols, vals, cb) {
-        console.log('burger.js')
-        orm.insertOne('burgers', cols, vals, function(res) {
-            // if (err) {
-            //     console.log(err);
-            // }
-            // console.log(res)
-            cb(res);
-        });
-    },
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne('burgers', objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
+    var Burger = sequelize.define("Burger", {
+
+        burgerName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        devoured: {
+            type: Datatypes.BOOLEAN,
+            defaultValue: false
+        },
+        createdAt: true,
+    });
+    return Burger;
 };
-
-// export burger model so other files can access it
-module.exports = burger;
