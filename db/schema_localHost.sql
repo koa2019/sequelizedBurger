ced397dc0cbbc62457be2b4737ff205af78281d7
koa2019 <no-reply>
2019-09-28 23:44:53 -0700

    configured schema_localHost.sql for sequelize

--- db/schema_localHost.sql
+++ db/schema_localHost.sql
@@ -1,10 +1,3 @@
-DROP DATABASE IF EXISTS `burger_db`;
-CREATE DATABASE `burger_db`;
-USE `burger_db`;
-CREATE TABLE `burgers` (
-    `id` INT AUTO_INCREMENT NOT NULL ,
-    `burgerName` VARCHAR(200) NOT NULL,
-    `devoured` BOOLEAN DEFAULT false NOT NULL,
-    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY(`id`)
-);
+DROP DATABASE IF EXISTS `burgerDb`;
+CREATE DATABASE `burgerDb`;
+USE `burgerDb`;
